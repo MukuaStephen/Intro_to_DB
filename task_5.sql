a13b493e2199df7c5b90d11b8b6c020a5425cb77
Stephen Mukua <mukuastephen08@gmail.com>
2024-11-11 00:21:39 +0300

    Intro to DB

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,7 @@
+
 -- Use the provided database (assumed to be passed as argument)
 USE alx_book_store;
 
--- Insert a single row into the Customers table
+-- Insert a single row into the Customers table with a unique customer_id
 INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES (2, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');

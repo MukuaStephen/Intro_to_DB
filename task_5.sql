88a9473a5e44e489b69c85e65cd6e85879ef75c0
Stephen Mukua <mukuastephen08@gmail.com>
2024-11-11 01:31:09 +0300

    changes

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,6 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Insert a single row into the customers table
 INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES (5, 'New Customer', 'newcustomer@sandtech.com', '127 Happiness Ave.');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');

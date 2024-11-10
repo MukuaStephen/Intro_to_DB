083b0a8e67a687aee396591a978bb463d44180be
Stephen Mukua <mukuastephen08@gmail.com>
2024-11-11 00:26:46 +0300

    Intro to DBS

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,9 @@
-
 -- Use the provided database (assumed to be passed as argument)
 USE alx_book_store;
 
--- Insert a single row into the Customers table with a unique customer_id
+-- Delete any existing customer with customer_id = 2
+DELETE FROM Customers WHERE customer_id = 2;
+
+-- Insert a single row into the Customers table
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (2, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
